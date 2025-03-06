ca0307553b52dd26d5682b9c7d81fbbf10a944db
ngphth1312 <ngphth1312@gmail.com>
2025-03-06 12:27:21 +0700

    fix: resolve merge conflict in PassChangeDialog.jsx when merging "thinh_branch" into "main"

--- src/components/dialog/PassChangeDialog.jsx
+++ src/components/dialog/PassChangeDialog.jsx
@@ -17,15 +17,14 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import {Input} from '@/components/ui/input';
+import {Alert, AlertDescription, AlertTitle} from '@/components/ui/alert';
 import {useChangePasswordMutation} from '@/services/auth';
 import {zodResolver} from '@hookform/resolvers/zod';
-import {DialogDescription} from '@radix-ui/react-dialog';
 import {AlertCircle, Eye, EyeOff} from 'lucide-react';
 import {useState} from 'react';
 import {useForm} from 'react-hook-form';
 import {toast} from 'sonner';
 import {z} from 'zod';
-import {Alert, AlertDescription, AlertTitle} from '../ui/alert';
 
 const PASSWORD_MIN_LENGTH = 6;
 
